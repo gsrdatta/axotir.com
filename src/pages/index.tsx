f6a75de7abc34c4c747da9cf721c4e5ff101574f
sivaramadatta.g <sivaramadatta.g@chakravuyha.com>
2022-09-22 10:04:26 +0530

    CHnages to name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,8 @@ export default function Home() {
   return (
     <Page>
       <NextSeo
-        title="STARTD - Template"
-        description="A TypeScript/Next.js theme that includes everything you need to build amazing landing page!"
+        title="Axotir"
+        description="A simple landing page!"
       />
       <Header />
       <main>
